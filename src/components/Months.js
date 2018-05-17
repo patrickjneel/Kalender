a91f2b6c4b3efb165a9a1ad76960f3bbf0d880b9
Pat Neel <patrick.neel@colorado.edu>
2018-05-17 14:54:00 -0600

    fixed days on calendar

--- src/components/Months.js
+++ src/components/Months.js
@@ -16,7 +16,7 @@ class Month extends Component {
     return JuneData.map((day, index) => {
       return (
         <div key={index} className="days-month">
-          <p>{day.day}</p>
+          <p class="individual-day">{day.day}</p>
           <p>{day.event}</p>
         </div>
       )
